66f3e19b0f735173771bb78c909c1d917d140a36
bluhhar <bluhhar@gmail.com>
2024-02-05 16:04:22 +0300

    +katas

---
+++ Fundamentals/Grasshopper - Summation/Main.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class Main {
+    public static int summation(int n) {
+        int i = 1;
+        int sum = 0;
+        while(i <= n) {
+            sum += i;
+            i++;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(summation(1));
+        System.out.println(summation(2));
+        System.out.println(summation(5));
+        System.out.println(summation(8));
+    }
+}
